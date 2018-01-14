be623f75e746046e62b932f4478353be00bf9e24
Ørjan Ellingsen <orjellingsen@gmail.com>
2018-01-14 23:06:49 +0100

    created categories actions

--- frontend/src/actions/categories.js
+++ frontend/src/actions/categories.js
@@ -1,3 +1,15 @@
 export const GET_CATEGORIES = 'GET_CATEGORIES' // GET /categories
-export const GET_CAREGORY_POST = 'GET_CATEGORY_POST' // GET /:category/posts
+export const GET_CAREGORY_POSTS = 'GET_CATEGORY_POSTS' // GET /:category/posts
 
+export function getCategories () {
+  return {
+    type: GET_CATEGORIES,
+  }
+}
+
+export function getCategoryPosts ({ category }) {
+  return {
+    type: GET_CATEGORY_POSTS,
+    category,
+  }
+}
