1d05ccf66c0500699bf628478feace758fc74928
Ørjan Ellingsen <orjellingsen@gmail.com>
2018-01-14 23:26:55 +0100

    install thunk

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,22 +7,14 @@ import reducer from './reducers'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import api from './utils/api_helper'
-
-const logger = store => next => action => {
-  console.group(action.type)
-  console.info('dispatching', action)
-  let result = next(action)
-  console.log('next state', store.getState())
-  console.groupEnd(action.type)
-  return result
-}
+import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
   reducer,
   composeEnhancers(
-    applyMiddleware(logger)
+    applyMiddleware(thunk)
   )
 )
 
